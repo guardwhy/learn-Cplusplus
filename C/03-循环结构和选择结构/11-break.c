a3e915c9ddee6bcd5a11b16adc1a480f259514a7
guardwhy <hxy1625309592@foxmail.com>
2020-08-19 17:41:47 +0800

    break

--- C/03-循环结构和选择结构/11-break.c
+++ C/03-循环结构和选择结构/11-break.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * break作用
+ * @return
+ */
+int main() {
+    // 定义变量
+    int waterLevel;
+    // 遍历条件
+    for(waterLevel = 10; waterLevel < 100; waterLevel+=5){
+        printf("当前的水位是%d\n", waterLevel);
+        // 条件判断
+        if(waterLevel == 95){
+            break;
+        }
+        sleep(1);
+    }
 
-int main()
-{
-    
+    printf("开阀放水。。\n");
     return 0;
 }
